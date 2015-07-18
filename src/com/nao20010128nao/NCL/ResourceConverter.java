a8a26a93eaf397e9796bcb2c86efe1924c958bc6
nao20010128nao <nao20010128@gmail.com>
2015-07-18 10:33:44 +0900

    追加

--- src/com/nao20010128nao/NCL/ResourceConverter.java
+++ src/com/nao20010128nao/NCL/ResourceConverter.java
@@ -2,6 +2,7 @@ package com.nao20010128nao.NCL;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.zip.GZIPInputStream;
 
 public abstract class ResourceConverter {
 
@@ -19,6 +20,15 @@ public abstract class ResourceConverter {
 				throws IOException {
 			// TODO 自動生成されたメソッド・スタブ
 			return is;
+		}
+	};
+	public static final ResourceConverter UNGZIP_CONVERTER = new ResourceConverter() {
+
+		@Override
+		public InputStream convert(InputStream is, String address)
+				throws IOException {
+			// TODO 自動生成されたメソッド・スタブ
+			return new GZIPInputStream(is);
 		}
 	};
 }
